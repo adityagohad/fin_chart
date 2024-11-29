e7fb2df95890e084ac94975adc2251e691efe2f4
Aditya Gohad <adityagohad@192.168.1.11>
2024-11-29 18:01:53 +0530

    project init

---
+++ example/lib/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Finance Charts"),
+      ),
+      body: const SafeArea(child: Placeholder()),
+    );
+  }
+}
