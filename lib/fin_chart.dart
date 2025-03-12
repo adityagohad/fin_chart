4e8da46492fb900b0ab0cecbbf18e8fb51cd32f0
adityagohad <adityagohad@gmail.com>
2025-03-13 03:57:30 +0530

    moved ui folder

--- lib/fin_chart.dart
+++ lib/fin_chart.dart
@@ -2,6 +2,7 @@ library fin_chart;
 
 export 'package:fin_chart/candle_stick_generator.dart';
 export 'package:fin_chart/chart.dart';
+export 'package:fin_chart/ui/region_dialog.dart';
 export 'package:fin_chart/models/i_candle.dart';
 export 'package:fin_chart/models/layers/rr_box.dart';
 export 'package:fin_chart/models/layers/smooth_line_data.dart';
